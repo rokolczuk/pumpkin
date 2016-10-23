bb23f336eded6d3ee594cad0c908ab9d6ee5b255
Andrzej Korolczuk <niepowiem@gmail.com>
2016-10-23 17:43:01 +0100

    open / close eyes loop

--- pumpkin.py
+++ pumpkin.py
@@ -7,7 +7,7 @@ from time import sleep
 pygame.mixer.init()
 pygame.mixer.music.set_volume(1)
 
-test = [
+eyes_open = [
     "0000000000000000",
     "0110000000000110",
     "1111100000011111",
@@ -17,6 +17,31 @@ test = [
     "0000000000000000",
     "0000000000000000"
 ]
+
+eyes_opening = [
+
+"0000000000000000",
+"0000000000000000",
+"0111100000011110",
+"0011110000111100",
+"0001111001111000",
+"0000000000000000",
+"0000000000000000",
+"0000000000000000"
+]
+
+eyes_closed = [
+
+"0000000000000000",
+"0000000000000000",
+"0000000000000000",
+"0011110000111100",
+"0001100000011000",
+"0000000000000000",
+"0000000000000000",
+"000000000000000"
+]
+
 
 device = led.matrix(cascaded=2)
 
@@ -32,9 +57,23 @@ def play_audio(path):
     pygame.mixer.music.load(path)
     pygame.mixer.music.play()
 
+def open_eyes():
 
-draw_matrix(test)
-play_audio("laugh.mp3")
+    draw_matrix(eyes_closed)
+    sleep(0.3)
+    draw_matrix(eyes_opening)
+    sleep(0.3)
+    draw_matrix(eyes_open)
+
+def close_eyes():
+    draw_matrix(eyes_open)
+    sleep(0.3)
+    draw_matrix(eyes_opening)
+    sleep(0.3)
+    draw_matrix(eyes_closed)
 
 while True:
-	sleep(1)
+    open_eyes()
+    sleep(5)
+    close_eyes()
+    sleep(5)
